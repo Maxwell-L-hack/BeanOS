9a0db5da1aab5216cbe53558d4451b4acd197ad5
Maxwell L <papalenhoff@gmail.com>
2026-06-06 18:36:35 -0500

    Wrote the boot sector

--- boot.asm
+++ boot.asm
@@ -1,8 +1,27 @@
-; Infinite loop (e9 fd ff)
-loop:
-    jmp loop
+[org 0x7c00]
+    mov bp, 0x8000
+    mov sp , bp
 
-; Fill with 510 zeros minus the size of the previous code
-times 510-($-$$) db 0
-; Magic number
+    mov bx, 0x9000
+    mov dh, 2 
+    call disk_load
+
+    mov dx, [0x9000]
+    call print_hex
+
+    call print_nl
+
+    mov dx, [0x9000 + 512]
+    call print_hex
+
+    jmp $
+
+%include "bootprint.asm"
+%include "bootprinthex.asm"
+%include "bootdisk.asm"
+
+times 510 - ($-$$) db 0
 dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
